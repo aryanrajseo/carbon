efba3e8a16a98b499db1742ef91fd10f6aedcd82
Aryan Raj <contact@aryanraj.com>
2023-03-28 13:03:43 +0530

    widget_init

--- inc/widgets.php
+++ inc/widgets.php
@@ -9,6 +9,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Register Sidebars.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+
 add_action( 'widgets_init', 'carbon_sidebar_init' );
 
 function carbon_sidebar_init() {
